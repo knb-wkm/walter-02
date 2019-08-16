bd93bdc4baa860443495bf9c183efb77f8da8042
K-YAMAGUCHI <quz@dummy.com>
2019-08-16 10:55:40 +0900

    add user_readonly into initData

--- server/lib/test/memmongo.js
+++ server/lib/test/memmongo.js
@@ -14,21 +14,13 @@ import Group from "../models/Group";
 let mongoServer;
 let mongoUri;
 
-// (async () => {
-//   mongoServer = new MongoMemoryServer();
-//   mongoUri = await mongoServer.getConnectionString();
-// })();
-
-// export const getConnectionString = () => {
-//   return mongoUri
-// }
-
 
 export const connect = async (tenant_name) => {
   mongoServer = new MongoMemoryServer();
   mongoUri = await mongoServer.getConnectionString();
+  //mongoUri = "mongodb://localhost:17017/walter" 
+  console.log(mongoUri)
   const opts = { useNewUrlParser: true };
-  //const mongoUri = getConnectionString()
   await mongoose.connect(mongoUri, opts, (err) => {
     if (err) { console.error(err); }
   });
@@ -39,6 +31,7 @@ export const connect = async (tenant_name) => {
   const initData = {}
   initData.tenant = (await Tenant.findOne({ name: tenant_name })).toObject()
   initData.user = (await User.findOne({ account_name: `${tenant_name}1` })).toObject()
+  initData.user_readonly = (await User.findOne({ account_name: `${tenant_name}2` })).toObject()
   initData.appSettings = (await AppSetting.find({ tenant_id: initData.tenant._id }))
   initData.roleFile = (await RoleFile.find({ tenant_id: initData.tenant._id }))
   initData.roleFileFull = initData.roleFile.filter(role => role.name === 'フルコントロール')[0].toObject()
